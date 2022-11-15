6915005c3da33b63eaca7650031552811674a125
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-15 01:31:23 +0000

    Add input.js

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 // play.js
 
 const connect = require("./client");
+const setupInput = require("./input")
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    // \u0003 maps to ctrl+c input
-    console.log("Thanks for using me, ciao!")
-    process.exit();
-  }
-};
 
 setupInput();
