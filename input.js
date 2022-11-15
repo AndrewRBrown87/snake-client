5bc5a7b0e276c3f26c2977da66025482f23b9e4d
Andrew Brown <andrew.robert.brown@gmail.com>
2022-11-15 02:19:17 +0000

    WASD Movement

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
 // input.js
 
-// setup interface to handle user input from stdin
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -17,6 +21,18 @@ const handleUserInput = function (key) {
     // \u0003 maps to ctrl+c input
     console.log("Thanks for using me, ciao!")
     process.exit();
+  } else if (key === '\u0077') {
+    // \u0077 maps to 'w' input
+    connection.write("Move: up");
+  } else if (key === '\u0061') {
+    // \u0061 maps to 'a' input
+    connection.write("Move: left");
+  } else if (key === '\u0073') {
+    // \u0073 maps to 's' input
+    connection.write("Move: down");
+  } else if (key === '\u0064') {
+    // \u0064 maps to 'd' input
+    connection.write("Move: right");
   }
 };
 
